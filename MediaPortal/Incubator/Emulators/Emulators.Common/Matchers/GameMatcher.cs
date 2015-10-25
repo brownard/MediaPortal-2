b2b02bab95e659346b6021535193d2e07bd4e821
brownard <brownard@live.co.uk>
2015-10-25 13:20:28 +0000

    Create IOnlineMatcher interface and implement in TheGamesDbWrapper

--- MediaPortal/Incubator/Emulators/Emulators.Common/Matchers/GameMatcher.cs
+++ MediaPortal/Incubator/Emulators/Emulators.Common/Matchers/GameMatcher.cs
@@ -1,12 +1,8 @@
 ﻿using Emulators.Common.Games;
-using Emulators.Common.NameProcessing;
 using Emulators.Common.TheGamesDb;
 using MediaPortal.Common;
-using MediaPortal.Common.Logging;
-using MediaPortal.Extensions.OnlineLibraries.Matches;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,13 +11,7 @@ namespace Emulators.Common.Matchers
 {
   public class GameMatcher
   {
-    protected static ILogger Logger
-    {
-      get { return ServiceRegistration.Get<ILogger>(); }
-    }
-
-    const int MAX_SEARCH_DISTANCE = 2;
-    TheGamesDbWrapper _theGamesDb = new TheGamesDbWrapper();
+    IOnlineMatcher _onlineMatcher = new TheGamesDbWrapper();
 
     public static GameMatcher Instance
     {
@@ -30,53 +20,7 @@ namespace Emulators.Common.Matchers
 
     public bool FindAndUpdateGame(GameInfo gameInfo)
     {
-      GameResult result;
-      if (!FindUniqueGame(gameInfo, out result))
-        return false;
-
-      Game game = result.Game;
-      gameInfo.GameName = game.GameTitle;
-      gameInfo.GamesDbId = game.Id;
-      gameInfo.Certification = game.ESRB;
-      gameInfo.Description = game.Overview;
-      gameInfo.Developer = game.Developer;
-      if (game.Genres != null && game.Genres.Genres != null)
-        gameInfo.Genres.AddRange(game.Genres.Genres);
-      gameInfo.Rating = game.Rating;
-      DateTime releaseDate;
-      if (DateTime.TryParse(game.ReleaseDate, TheGamesDbWrapper.DATE_CULTURE, DateTimeStyles.None, out releaseDate))
-        gameInfo.ReleaseDate = releaseDate;
-      if (game.Id > 0)
-        _theGamesDb.ScheduleDownload(game.Id);
-      return true;
-    }
-
-    protected bool FindUniqueGame(GameInfo gameInfo, out GameResult result)
-    {
-      result = null;
-      NameProcessor.CleanupTitle(gameInfo);
-      if (_theGamesDb.TryGetFromStorage(gameInfo.GameName, gameInfo.Platform, out result))
-      {
-        Logger.Debug("GameMatcher: Retrieved from cache: '{0}' - '{1}'", gameInfo.GameName, gameInfo.Platform);
-        return true;
-      }
-
-      List<GameSearchResult> results;
-      if (!_theGamesDb.Search(gameInfo.GameName, gameInfo.Platform, out results))
-      {
-        Logger.Debug("GameMatcher: No results found for game: '{0}' - '{1}'", gameInfo.GameName, gameInfo.Platform);
-        return false;
-      }
-
-      Logger.Debug("GameMatcher: Found {0} items for game: '{1}' - '{2}'", results.Count, gameInfo.GameName, gameInfo.Platform);
-      results = results.FindAll(r => r.GameTitle == gameInfo.GameName || NameProcessor.GetLevenshteinDistance(r.GameTitle, gameInfo.GameName) <= MAX_SEARCH_DISTANCE);
-      if (results.Count > 0 && _theGamesDb.Get(results[0].Id, out result))
-      {
-        Logger.Debug("GameMatcher: Matched '{0}' to game: '{1}' - '{2}'", results[0].GameTitle, gameInfo.GameName, gameInfo.Platform);
-        _theGamesDb.AddToStorage(gameInfo.GameName, gameInfo.Platform, result.Game.Id);
-        return true;
-      }
-      return false;
+      return _onlineMatcher.TryGetBestMatch(gameInfo);
     }
   }
 }
---
+++ MediaPortal/Incubator/Emulators/Emulators.Common/Matchers/IOnlineMatcher.cs
@@ -0,0 +1,14 @@
+﻿using Emulators.Common.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulators.Common.Matchers
+{
+  interface IOnlineMatcher
+  {
+    bool TryGetBestMatch(GameInfo gameInfo);
+  }
+}
