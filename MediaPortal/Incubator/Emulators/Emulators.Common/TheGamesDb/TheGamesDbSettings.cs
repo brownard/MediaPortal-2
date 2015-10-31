12046f4092fee432b6f8319307f140fe3fffd27d
brownard <brownard@live.co.uk>
2015-10-31 23:34:16 +0000

    Add new image types and download all available images from TheGamesDB

---
+++ MediaPortal/Incubator/Emulators/Emulators.Common/TheGamesDb/TheGamesDbSettings.cs
@@ -0,0 +1,15 @@
+﻿using MediaPortal.Common.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulators.Common.TheGamesDb
+{
+  public class TheGamesDbSettings
+  {
+    [Setting(SettingScope.Global, 0)]
+    public int APIVersion { get; set; }
+  }
+}
