5c1c18c658267d6e33f105ae3247d366c9370137
brownard <brownard@live.co.uk>
2015-12-30 08:40:55 +0000

    Only recreate textures when necessary

--- MediaPortal/Incubator/Emulators/Emulators/LibRetro/VideoProviders/LibRetroTextureWrapper.cs
+++ MediaPortal/Incubator/Emulators/Emulators/LibRetro/VideoProviders/LibRetroTextureWrapper.cs
@@ -10,7 +10,8 @@ namespace Emulators.LibRetro.VideoProviders
 {
   public class LibRetroTextureWrapper : ITextureProvider
   {
-    protected Texture[] _textures = new Texture[2];
+    const int TEXTURE_BUFFER_LENGTH = 2;
+    protected Texture[] _textures = new Texture[TEXTURE_BUFFER_LENGTH];
     protected int _currentTextureIndex;
 
     public Texture Texture
@@ -20,17 +21,17 @@ namespace Emulators.LibRetro.VideoProviders
 
     public void UpdateTexture(Device device, int[] pixels, int width, int height)
     {
+      if (pixels == null)
+        return;
       _currentTextureIndex = (_currentTextureIndex + 1) % _textures.Length;
-      Texture texture = new Texture(device, width, height, 1, Usage.Dynamic, Format.X8R8G8B8, Pool.Default);
+      Texture texture = GetOrCreateTexture(device, width, height);
       DataStream dataStream;
       texture.LockRectangle(0, LockFlags.None, out dataStream);
       using (dataStream)
+      {
         dataStream.WriteRange(pixels, 0, width * height);
-      texture.UnlockRectangle(0);
-      Texture oldTexture = _textures[_currentTextureIndex];
-      if (oldTexture != null)
-        oldTexture.Dispose();
-      _textures[_currentTextureIndex] = texture;
+        texture.UnlockRectangle(0);
+      }
     }
 
     public void UpdateTexture(Device device, byte[] pixels, int width, int height, bool bottomLeftOrigin)
@@ -38,7 +39,7 @@ namespace Emulators.LibRetro.VideoProviders
       if (pixels == null)
         return;
       _currentTextureIndex = (_currentTextureIndex + 1) % _textures.Length;
-      Texture texture = new Texture(device, width, height, 1, Usage.Dynamic, Format.X8R8G8B8, Pool.Default);
+      Texture texture = GetOrCreateTexture(device, width, height);
       DataStream dataStream;
       texture.LockRectangle(0, LockFlags.None, out dataStream);
       using (dataStream)
@@ -47,12 +48,8 @@ namespace Emulators.LibRetro.VideoProviders
           FlipVertically(pixels, width, height, dataStream);
         else
           dataStream.WriteRange(pixels, 0, width * height * 4);
+        texture.UnlockRectangle(0);
       }
-      texture.UnlockRectangle(0);
-      Texture oldTexture = _textures[_currentTextureIndex];
-      if (oldTexture != null)
-        oldTexture.Dispose();
-      _textures[_currentTextureIndex] = texture;
     }
 
     protected void FlipVertically(byte[] pixels, int width, int height, DataStream dataStream)
@@ -65,6 +62,27 @@ namespace Emulators.LibRetro.VideoProviders
         dataStream.WriteRange(pixels, offset, byteWidth);
         currentRow--;
       }
+    }
+
+    protected Texture GetOrCreateTexture(Device device, int width, int height)
+    {
+      Texture texture = _textures[_currentTextureIndex];
+      bool needsCreation = texture == null || texture.IsDisposed;
+      if (!needsCreation)
+      {
+        SurfaceDescription surface = texture.GetLevelDescription(0);
+        if (surface.Width != width || surface.Height != height)
+        {
+          texture.Dispose();
+          needsCreation = true;
+        }
+      }
+      if (needsCreation)
+      {
+        texture = new Texture(device, width, height, 1, Usage.Dynamic, Format.X8R8G8B8, Pool.Default);
+        _textures[_currentTextureIndex] = texture;
+      }
+      return texture;
     }
 
     public void Release()
