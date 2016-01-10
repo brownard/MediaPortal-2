64ca3c3788a43dca646aebc5a15ec6040bd442f9
brownard <brownard@live.co.uk>
2016-01-10 23:55:30 +0000

    Add libretro settings class

---
+++ MediaPortal/Incubator/Emulators/Emulators/LibRetro/Settings/LibRetroSettings.cs
@@ -0,0 +1,21 @@
+﻿using MediaPortal.Common.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulators.LibRetro.Settings
+{
+  public class LibRetroSettings
+  {
+    [Setting(SettingScope.User, 4)]
+    public int MaxPlayers { get; set; }
+
+    [Setting(SettingScope.User, true)]
+    public bool AutoSave { get; set; }
+
+    [Setting(SettingScope.User, 30)]
+    public int AutoSaveInterval { get; set; }
+  }
+}
