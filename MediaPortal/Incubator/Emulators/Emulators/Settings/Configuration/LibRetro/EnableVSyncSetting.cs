cfa21b0af28f71f20bdf9539c7681c30979a1619
brownard <brownard@live.co.uk>
2016-01-24 21:19:41 +0000

    Add VSync setting to GUI

---
+++ MediaPortal/Incubator/Emulators/Emulators/Settings/Configuration/LibRetro/EnableVSyncSetting.cs
@@ -0,0 +1,25 @@
+﻿using Emulators.LibRetro.Settings;
+using MediaPortal.Common.Configuration.ConfigurationClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulators.Settings.Configuration.LibRetro
+{
+  public class EnableVSyncSetting : YesNo
+  {
+    public override void Load()
+    {
+      _yes = SettingsManager.Load<LibRetroSettings>().EnableVSync;
+    }
+
+    public override void Save()
+    {
+      LibRetroSettings settings = SettingsManager.Load<LibRetroSettings>();
+      settings.EnableVSync = _yes;
+      SettingsManager.Save(settings);
+    }
+  }
+}
