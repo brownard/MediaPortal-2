5d7c0bce2a84aa64e555624cb2deb4ef116998a0
brownard <brownard@live.co.uk>
2016-02-05 21:14:18 +0000

    Begin implementing core info handler

---
+++ MediaPortal/Incubator/Emulators/Emulators/LibRetro/Cores/CoreInfoHandler.cs
@@ -0,0 +1,112 @@
+﻿using Emulators.Common.WebRequests;
+using MediaPortal.Common;
+using MediaPortal.Common.Logging;
+using SharpRetro.Info;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulators.LibRetro.Cores
+{
+  class CoreInfoHandler
+  {
+    protected const string BASE_URL = "http://buildbot.libretro.com";
+    protected const string INFO_URL = "/assets/frontend/info/";
+
+    protected string _infoDirectory;
+    protected HtmlDownloader _downloader;
+    protected List<CoreInfo> _coreInfos;
+
+    public CoreInfoHandler(string infoDirectory)
+    {
+      _infoDirectory = infoDirectory;
+      _downloader = new HtmlDownloader();
+      _coreInfos = new List<CoreInfo>();
+    }
+
+    public void Update()
+    {
+      DownloadCoreInfos();
+      LoadCoreInfos();
+    }
+
+    public void Load()
+    {
+      LoadCoreInfos();
+    }
+
+    protected void DownloadCoreInfos()
+    {
+      if (!TryCreateInfoDirectory())
+        return;
+
+      CoreInfoList infoList = _downloader.Download<CoreInfoList>(BASE_URL + INFO_URL);
+      if (infoList == null)
+        return;
+
+      foreach (string infoUrl in infoList.CoreInfoUrls)
+      {
+        Uri uri;
+        if (!Uri.TryCreate(infoUrl, UriKind.RelativeOrAbsolute, out uri))
+          continue;
+        string url = uri.IsAbsoluteUri ? infoUrl : BASE_URL + infoUrl;
+        string path = Path.Combine(_infoDirectory, Path.GetFileName(uri.LocalPath));
+        _downloader.DownloadFile(url, path, true);
+      }
+    }
+
+    protected void LoadCoreInfos()
+    {
+      _coreInfos = new List<CoreInfo>();
+      if (!Directory.Exists(_infoDirectory))
+        return;
+
+      try
+      {
+        foreach (string file in Directory.EnumerateFiles(_infoDirectory, "*.info"))
+        {
+          CoreInfo coreInfo;
+          if (TryLoadCoreInfo(file, out coreInfo))
+            _coreInfos.Add(coreInfo);
+        }
+      }
+      catch (Exception ex)
+      {
+        ServiceRegistration.Get<ILogger>().Error("CoreInfoHandler: Exception reading directory '{0}'", ex, _infoDirectory);
+      }
+    }
+
+    protected bool TryLoadCoreInfo(string path, out CoreInfo coreInfo)
+    {
+      try
+      {
+        string text = File.ReadAllText(path);
+        coreInfo = new CoreInfo(text);
+        return coreInfo.DisplayName != null;
+      }
+      catch (Exception ex)
+      {
+        ServiceRegistration.Get<ILogger>().Error("CoreInfoHandler: Exception loading info from '{0}'", ex, path);
+      }
+      coreInfo = null;
+      return false;
+    }
+
+    protected bool TryCreateInfoDirectory()
+    {
+      try
+      {
+        Directory.CreateDirectory(_infoDirectory);
+        return true;
+      }
+      catch (Exception ex)
+      {
+        ServiceRegistration.Get<ILogger>().Error("CoreInfoHandler: Exception creating info directory '{0}'", ex, _infoDirectory);
+      }
+      return false;
+    }
+  }
+}
