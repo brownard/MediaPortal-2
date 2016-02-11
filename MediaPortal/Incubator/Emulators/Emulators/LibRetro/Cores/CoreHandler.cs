1aa2ee627b02c0c9ba7bba81f986b782d08c192a
brownard <brownard@live.co.uk>
2016-02-11 23:06:53 +0000

    Begin implementing core updater model

---
+++ MediaPortal/Incubator/Emulators/Emulators/LibRetro/Cores/CoreHandler.cs
@@ -0,0 +1,79 @@
+﻿using Emulators.Common.GoodMerge;
+using Emulators.Common.WebRequests;
+using MediaPortal.Common;
+using MediaPortal.Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulators.LibRetro.Cores
+{
+  public class CoreHandler
+  {
+    protected const string BASE_URL = "http://buildbot.libretro.com";
+    protected const string LATEST_URL = "/nightly/windows/x86/latest/";
+
+    protected string _coresDirectory;
+    protected List<CoreUrl> _onlineCores;
+    protected HtmlDownloader _downloader;
+
+    public CoreHandler(string coresDirectory)
+    {
+      _coresDirectory = coresDirectory;
+      _downloader = new HtmlDownloader();
+      _onlineCores = new List<CoreUrl>();
+    }
+
+    public List<CoreUrl> OnlineCores
+    {
+      get { return _onlineCores; }
+    }
+
+    public void Update()
+    {
+      CoreList coreList = _downloader.Download<CoreList>(BASE_URL + LATEST_URL);
+      if (coreList != null)
+        _onlineCores = coreList.CoreUrls;
+    }
+
+    public void DownloadCore(CoreUrl coreUrl)
+    {
+      if (!TryCreateCoresDirectory())
+        return;
+
+      Uri uri;
+      if (!Uri.TryCreate(coreUrl.Url, UriKind.RelativeOrAbsolute, out uri))
+        return;
+      
+      string url = uri.IsAbsoluteUri ? coreUrl.Url : BASE_URL + coreUrl.Url;
+      string path = Path.Combine(_coresDirectory, Path.GetFileName(coreUrl.Name));
+      _downloader.DownloadFile(url, path, true);
+      ExtractCore(path);
+    }
+
+    protected void ExtractCore(string path)
+    {
+      IExtractor extractor = ExtractorFactory.Create(path);
+      if (!extractor.IsArchive())
+        return;
+      extractor.ExtractAll(_coresDirectory);
+    }
+
+    protected bool TryCreateCoresDirectory()
+    {
+      try
+      {
+        Directory.CreateDirectory(_coresDirectory);
+        return true;
+      }
+      catch (Exception ex)
+      {
+        ServiceRegistration.Get<ILogger>().Error("CoreHandler: Exception creating cores directory '{0}'", ex, _coresDirectory);
+      }
+      return false;
+    }
+  }
+}
---
+++ MediaPortal/Incubator/Emulators/Emulators/Models/LibRetroCoreUpdaterModel.cs
@@ -0,0 +1,79 @@
+﻿using Emulators.LibRetro.Cores;
+using MediaPortal.Common;
+using MediaPortal.Common.PathManager;
+using MediaPortal.UI.Presentation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaPortal.UI.Presentation.Workflow;
+
+namespace Emulators.Models
+{
+  public class LibRetroCoreUpdaterModel : IWorkflowModel
+  {
+    public static readonly Guid MODEL_ID = new Guid("656E3AC1-0363-4DA9-A23F-F1422A9ADD74");
+    protected static readonly string CORE_DIRECTORY = ServiceRegistration.Get<IPathManager>().GetPath(@"<DATA>\LibRetro\Cores\");
+    protected CoreHandler _coreHandler;
+
+    public LibRetroCoreUpdaterModel()
+    {
+      _coreHandler = new CoreHandler(CORE_DIRECTORY);
+    }
+
+    protected void Update()
+    {
+      _coreHandler.Update();
+      if (_coreHandler.OnlineCores.Count > 0)
+        _coreHandler.DownloadCore(_coreHandler.OnlineCores[0]);
+    }
+
+    #region IWorkflow
+    public Guid ModelId
+    {
+      get { return MODEL_ID; }
+    }
+
+    public bool CanEnterState(NavigationContext oldContext, NavigationContext newContext)
+    {
+      return true;
+    }
+
+    public void ChangeModelContext(NavigationContext oldContext, NavigationContext newContext, bool push)
+    {
+      
+    }
+
+    public void Deactivate(NavigationContext oldContext, NavigationContext newContext)
+    {
+      
+    }
+
+    public void EnterModelContext(NavigationContext oldContext, NavigationContext newContext)
+    {
+      Update();
+    }
+
+    public void ExitModelContext(NavigationContext oldContext, NavigationContext newContext)
+    {
+      
+    }
+
+    public void Reactivate(NavigationContext oldContext, NavigationContext newContext)
+    {
+      
+    }
+
+    public void UpdateMenuActions(NavigationContext context, IDictionary<Guid, WorkflowAction> actions)
+    {
+      
+    }
+
+    public ScreenUpdateMode UpdateScreen(NavigationContext context, ref string screen)
+    {
+      return ScreenUpdateMode.AutoWorkflowManager;
+    }
+    #endregion
+  }
+}
