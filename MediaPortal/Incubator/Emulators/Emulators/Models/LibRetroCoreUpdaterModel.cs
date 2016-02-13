b920d15c5fc60341e8b6ecd4062b4e23cfc1eea7
brownard <brownard@live.co.uk>
2016-02-13 14:02:16 +0000

    Rework async update dialogs

--- MediaPortal/Incubator/Emulators/Emulators/Models/LibRetroCoreUpdaterModel.cs
+++ MediaPortal/Incubator/Emulators/Emulators/Models/LibRetroCoreUpdaterModel.cs
@@ -30,22 +30,23 @@ namespace Emulators.Models
     public const string KEY_CORE_INFO = "LibRetro: CoreInfo";
     public const string KEY_CORE = "LibRetro: Core";
     public const string DIALOG_CORE_UPDATE_PROGRESS = "dialog_core_update_progress";
-    
+
     protected AbstractProperty _progressLabelProperty = new WProperty(typeof(string), null);
 
     protected readonly object _updateSync = new object();
+    protected readonly object _downloadSync = new object();
     protected string _coresDirectory;
     protected string _infoDirectory;
     protected CoreHandler _coreHandler;
     protected CoreInfoHandler _infoHandler;
     protected ItemsList _coreItems;
-    protected SynchronizedCollection<string> _downloadingUrls;
+    protected HashSet<string> _downloadingUrls;
     protected bool _isUpdating;
 
     public LibRetroCoreUpdaterModel()
     {
       _coreItems = new ItemsList();
-      _downloadingUrls = new SynchronizedCollection<string>();      
+      _downloadingUrls = new HashSet<string>();
       _coreHandler = new CoreHandler();
       _infoHandler = new CoreInfoHandler();
     }
@@ -68,28 +69,39 @@ namespace Emulators.Models
 
     protected void DownloadCoreAsync(LibRetroCoreItem item, LocalCore core)
     {
-      ServiceRegistration.Get<IThreadPool>().Add(() => DownloadCore(item, core));
+      var sm = ServiceRegistration.Get<IScreenManager>();
+      Guid? dialogId = null;
+      ServiceRegistration.Get<IThreadPool>().Add(() =>
+      {
+        ProgressLabel = LocalizationHelper.Translate("[Emulators.CoreUpdater.Downloading]", core.CoreName);
+        dialogId = sm.ShowDialog(DIALOG_CORE_UPDATE_PROGRESS);
+        DownloadCore(item, core);
+      },
+      e =>
+      {
+        if (dialogId.HasValue)
+          sm.CloseDialog(dialogId.Value);
+      });
     }
 
     protected void DownloadCore(LibRetroCoreItem item, LocalCore core)
-    {      
-      if (_downloadingUrls.Contains(core.Url))
-        return;
+    {
+      lock (_downloadSync)
+      {
+        if (_downloadingUrls.Contains(core.Url))
+          return;
+        _downloadingUrls.Add(core.Url);
+      }
 
       try
       {
-        ProgressLabel = LocalizationHelper.Translate("[Emulators.CoreUpdater.Downloading]", core.CoreName);
-        var sm = ServiceRegistration.Get<IScreenManager>();
-        Guid? dialogId = sm.ShowDialog(DIALOG_CORE_UPDATE_PROGRESS);
         if (_coreHandler.DownloadCore(core, _coresDirectory))
           item.Downloaded = true;
-
-        if (dialogId.HasValue)
-          sm.CloseDialog(dialogId.Value);
       }
       finally
       {
-        _downloadingUrls.Remove(core.Url);
+        lock (_downloadSync)
+          _downloadingUrls.Remove(core.Url);
       }
     }
 
@@ -98,7 +110,20 @@ namespace Emulators.Models
       LibRetroSettings settings = ServiceRegistration.Get<ISettingsManager>().Load<LibRetroSettings>();
       _coresDirectory = settings.CoresDirectory;
       _infoDirectory = settings.InfoDirectory;
-      ServiceRegistration.Get<IThreadPool>().Add(Update);
+
+      var sm = ServiceRegistration.Get<IScreenManager>();
+      Guid? dialogId = null;
+      ServiceRegistration.Get<IThreadPool>().Add(() =>
+      {
+        ProgressLabel = "[Emulators.CoreUpdater.UpdatingCores]";
+        dialogId = sm.ShowDialog(DIALOG_CORE_UPDATE_PROGRESS);
+        Update();
+      },
+      e =>
+      {
+        if (dialogId.HasValue)
+          sm.CloseDialog(dialogId.Value);
+      });
     }
 
     protected void Update()
@@ -112,17 +137,9 @@ namespace Emulators.Models
 
       try
       {
-        ProgressLabel = "[Emulators.CoreUpdater.UpdatingInfo]";
-        var sm = ServiceRegistration.Get<IScreenManager>();
-        Guid? dialogId = sm.ShowDialog(DIALOG_CORE_UPDATE_PROGRESS);
-
         _infoHandler.Update(_infoDirectory);
-        ProgressLabel = "[Emulators.CoreUpdater.UpdatingCores]";
         _coreHandler.Update();
         RebuildItemsList();
-
-        if (dialogId.HasValue)
-          sm.CloseDialog(dialogId.Value);
       }
       finally
       {
@@ -133,7 +150,7 @@ namespace Emulators.Models
 
     protected void RebuildItemsList()
     {
-      string coresDirectory = _coresDirectory; 
+      string coresDirectory = _coresDirectory;
       _coreItems.Clear();
       foreach (LocalCore core in _coreHandler.Cores)
         _coreItems.Add(CreateListItem(core, coresDirectory));
@@ -175,12 +192,12 @@ namespace Emulators.Models
 
     public void ChangeModelContext(NavigationContext oldContext, NavigationContext newContext, bool push)
     {
-      
+
     }
 
     public void Deactivate(NavigationContext oldContext, NavigationContext newContext)
     {
-      
+
     }
 
     public void EnterModelContext(NavigationContext oldContext, NavigationContext newContext)
@@ -190,17 +207,17 @@ namespace Emulators.Models
 
     public void ExitModelContext(NavigationContext oldContext, NavigationContext newContext)
     {
-      
+
     }
 
     public void Reactivate(NavigationContext oldContext, NavigationContext newContext)
     {
-      
+
     }
 
     public void UpdateMenuActions(NavigationContext context, IDictionary<Guid, WorkflowAction> actions)
     {
-      
+
     }
 
     public ScreenUpdateMode UpdateScreen(NavigationContext context, ref string screen)
